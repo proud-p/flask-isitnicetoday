4b3de132db6f4d263d63bb316376f14f5741b01b
proud-p <avika.pulges@hogarth.com>
2024-12-06 13:19:14 +0000

    connected front to back end
    backend able to retreive location data from frontend
    
    TODO: return output from back to front

--- app/app.py
+++ app/app.py
@@ -1,28 +1,37 @@
 from flask import *
 import requests
+import dotenv
+import os
+
 app = Flask(__name__)
 
-def get_ip():
-    response = requests.get('https://api64.ipify.org?format=json').json()
-    return response["ip"]
+dotenv.load_dotenv()
 
-def get_location():
-	ip_address = get_ip()
-	response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
-	location_data = {
-        "ip": ip_address,
-        "city": response.get("city"),
-        "region": response.get("region"),
-    	"country": response.get("country_name")
-	}
-    
-	print(location_data)
-    
-	return location_data
-@app.route("/")
-def home():
-    data = get_location()
-    return render_template("index.html",data=data)
+# Your API key
+api_key = os.getenv("GOOGLE_PLACES_KEY")
+
+# Define the endpoint
+endpoint_url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json"
+
+
+
+@app.route("/location", methods=["POST"])
+def index_location():
+    print("JUST CHECKING")
+    data = request.get_json()
+    latitude = data.get('latitude')
+    longitude = data.get('longitude')
+    print(latitude,longitude)
+
+    return render_template("index.html")
+        
+@app.route("/", methods=["GET","POST"])
+def index():
+    return render_template("index.html")
+
+# TODO Scrape wiki netflix and disney plus - filter by area
+# TODO comfy UI as API?
+# TODO connect to weather API
 
 if __name__ == "__main__":
     app.run(debug=True)
